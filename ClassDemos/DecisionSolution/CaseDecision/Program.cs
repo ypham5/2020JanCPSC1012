49d4674f8597ad214a958e033e6d0ffbc7e216e4
ypham5 <ypham5@nait.ca>
2020-01-29 18:51:01 -0700

    done

---
+++ ClassDemos/DecisionSolution/CaseDecision/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseDecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gradeLetter = "c";
+            string percentageRange;
+
+            //a case structure can be used when you have a 
+            //single argument value compared to 
+            //a series of literal values
+
+            //the structure bwgins with switch (argument value) {...}
+            //within the switch structure you have "cases"
+            //a "case" represents on "if" test against the argument value 
+            //the case can must end with a "break;" command
+            //the "break" sends you execution to the end of the switch 
+            //  structure
+            //the last case in your switch is a special case called "defaulf"
+            //"defaut" DOES NOT test against a literal value
+            //default executes if NONE of the previous cases were executed
+            //you can have multiple case statements code for a particular 
+            //case coding block
+            //multiple case statements for a particular case coding block are 
+            //implied logical OR (||) testss
+            //the case test is a relational equal (==) test
+            //switch(gradeLetter)
+            //{
+            //    case "a":
+            //    case "A":
+            //        {
+            //            //logic for this particular case
+            //            percentageRange = "85 - 100";
+            //            break;
+            //        }
+            //    case "b":
+            //    case "B":
+            //        {
+            //            //logic for this particular case
+            //            percentageRange = "70 - 84";
+            //            break;
+            //        }
+            //    case "c":
+            //    case "C":
+            //        {
+            //            //logic for this particular case
+            //            percentageRange = "55 - 69";
+            //            break;
+            //        }
+            //    case "d":
+            //    case "D":
+            //        {
+            //            //logic for this particular case
+            //            percentageRange = "40 - 54";
+            //            break;
+            //        }
+            //    case "f":
+            //    case "F":
+            //        {
+            //            //logic for this particular case
+            //            percentageRange = "0-39";
+            //            break;
+            //        }
+            //    default:
+            //        {
+            //            //the implied test on this case is "None of the about"
+            //            percentageRange = "Invalid enter grade";
+            //            break;
+            //        }
+            //}//eos
+
+            switch (gradeLetter.ToUpper())
+            {
+                case "A":
+                    {
+                        //logic for this particular case
+                        percentageRange = "85 - 100";
+                        break;
+                    }
+                case "B":
+                    {
+                        //logic for this particular case
+                        percentageRange = "70 - 84";
+                        break;
+                    }
+                case "C":
+                    {
+                        //logic for this particular case
+                        percentageRange = "55 - 69";
+                        break;
+                    }
+                case "D":
+                    {
+                        //logic for this particular case
+                        percentageRange = "40 - 54";
+                        break;
+                    }
+                case "F":
+                    {
+                        //logic for this particular case
+                        percentageRange = "0-39";
+                        break;
+                    }
+                default:
+                    {
+                        //the implied test on this case is "None of the about"
+                        percentageRange = "Invalid enter grade";
+                        break;
+                    }
+            }//eos
+            Console.WriteLine($"Your grade letter ({gradeLetter}) is {percentageRange}");
+            Console.ReadKey();//When using debugging to spot the auto closing 
+        }
+
+    }
+}
--- Practising-exercises/Decision-exercise-01/Program.cs
+++ Practising-exercises/Decision-exercise-01/Program.cs
@@ -10,9 +10,12 @@ namespace Decision_exercise_01
     {
         static void Main(string[] args)
         {
+            //Exercise-01
+            string inputValue;
+            double number;
             Console.Write("Enter your number:");
-            string inputValue = Console.ReadLine();
-            double number = double.Parse(inputValue);
+            inputValue = Console.ReadLine();
+            number = double.Parse(inputValue);
             if (number>=0)
             {
                 Console.WriteLine($"Your number {number} is possitive");
@@ -21,7 +24,82 @@ namespace Decision_exercise_01
             {
                 Console.WriteLine($"Your number {number} is negative");
             }
-            Console.ReadLine();                                                                     
+            Console.ReadLine();
+            //Exercise-02
+            string inputAge, msg;
+            double age;
+            Console.Write("Enter your age:");
+            inputAge = Console.ReadLine();
+            age = double.Parse(inputAge);
+
+            if (age <=6)
+            {
+                msg = "Free";
+            }
+            else if ( age >= 7 && age <=17)
+            {
+                msg = "$9.80";
+            }
+            else if (age >= 18 && age <= 54)
+            {
+                msg = "$11.35";
+            }
+            else
+            {
+                msg = "$10.00";
+            }
+            Console.WriteLine(msg);
+            Console.ReadLine();
+            //Exercise03
+            string inputMark, grademsg;
+            double mark;
+            Console.Write("Enter your Mark:");
+            inputMark = Console.ReadLine();
+            mark = double.Parse(inputMark);
+
+            if (mark<= 49)
+            {
+                grademsg = "Your Grade is F";
+            }
+            else if (mark >= 50 && mark <= 69)
+            {
+                grademsg = "Your Grade is D";
+            }
+            else if (mark >= 70 && mark <= 79)
+            {
+                grademsg = "Your Grade is C";
+            }
+            else if (mark >= 80 && mark <= 89)
+            {
+                grademsg = "Your Grade is B";
+            }
+            else
+            {
+                grademsg = "Your Grade is A";
+            }
+            Console.WriteLine(grademsg);
+            Console.ReadLine();
+            //Exercise04
+            string inputIncome;
+            double tax, income;
+            Console.Write("Enter your taxable income:");
+            inputIncome = Console.ReadLine();
+            income = double.Parse(inputIncome);
+
+            if ( income <=50000)
+            {
+                tax = 0 + 5 / 100 * income;
+            }
+            else if (income <=100000)
+            {
+                tax = 2500 + 7 / 100 * income;
+            }
+            else
+            {
+                tax = 6000 + 9 / 100 * income;
+            }
+            Console.WriteLine($"Your income tax is ${tax}");
+            Console.ReadLine();
         }
     }
 }
