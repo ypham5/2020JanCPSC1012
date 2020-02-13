8a69a13b53e38bd31a0f9d4d151c2e41f1520b23
ypham5 <ypham5@nait.ca>
2020-02-12 18:26:47 -0700

    demo done

---
+++ ClassDemos/MenuLoop-solution/MenuLoop/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //generic menu setup
+            //loop: post-loop
+            //loop contents
+            //      prompt for menu option
+            //      switch with case for each option, default is bad menu option
+            //      within the case we will simple out a message
+            //decide how to terminate your loop
+            string menuOption ="";
+
+            do
+            {
+                //prompts
+                Console.WriteLine("Select a option from the following option:");
+                Console.WriteLine("A) Do case A");
+                Console.WriteLine("B) Do case B");
+                Console.WriteLine("C) Do case C");
+                Console.WriteLine("X) Exit\n");
+                Console.WriteLine("Entry your menu option:\t");
+                menuOption = Console.ReadLine();
+
+                switch(menuOption.ToUpper())
+                {
+                    case "A":
+                        {
+                            //place any logic to do in case A
+                            Console.WriteLine("Your entered menu option choice of A \n\n");
+                            break;
+                        }
+                    case "B":
+                        {
+                            Console.WriteLine("Your entered menu option choice of B \n\n");
+                            break;
+                        }
+                    case "C":
+                        {
+                            Console.WriteLine("Your entered menu option choice of C \n\n");
+                            break;
+                        }
+                    case "X":
+                        {
+                            Console.WriteLine($"Thank you. Come again.\n\n");
+                            break;
+                        }
+                    default:
+                        {
+                            //this case acts as a varidation for the proper value of a menuOption
+                            Console.WriteLine($@"Your menu option choice of {menuOption} is incorrect. Try again." +"\n\n");
+                            break;
+                        }
+                }
+            } while (menuOption.ToUpper() != "X");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassDemos/MenuLoop-solution/NestedLoop/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //query user for a number 
+            //validate a number was entered (integer) 
+            //if a number was entered check for even/odd
+            //continue unless a 0 was entered then exit
+
+            //to validate a string to contain a number 
+            // use the xxxx.TryParse of the number datatype 
+            // syntax of .TryParse(string, out variablename)
+            // string hold the value to test
+            // if the value in good: 
+            //      the number is automatically place in your variable 
+            //      the test returns a boolean true 
+            // if the value is not good: 
+            // NO value is placed in your variable 
+            // the test returns a boolean false
+
+            int number = -1;
+            string inputString = "";
+
+            while (number !=0)
+            {
+                //Console.WriteLine("Enter a positive whole number OR enter 0 to quit.");
+                //nputString = Console.ReadLine();
+
+                //} while (!int.TryParse(inputString, out number)) ;
+
+                //condition flag 
+                //NOTE: it MUST be reset on EACH pass of the OUTER loop
+                bool validFlag = false; 
+                do
+                {
+                    Console.WriteLine("Enter a positive whole number OR enter 0 to quit.");
+                    inputString = Console.ReadLine();
+                    //need a test to appropriately set your loop exit test
+                    if (int.TryParse(inputString, out number))
+                    {
+                        //set the flag to an appropriate value to work with your logic
+                        validFlag = true;
+                    }
+                } while (validFlag == false);
+
+                if (number>0)
+                {
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine($"{number} is an even value.\n\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number} is an odd value.\n\n");
+                    }
+                }
+                else
+                {
+                    if (number == 0)
+                    {
+                        Console.WriteLine("Thank you. Come again.\n\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number} is an invalid. Try again.\n\n");
+                    }
+                    Console.ReadLine();
+                
+                }
+            }
+        }
+    }
+}
