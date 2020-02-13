dd4bcd1b3d842aa2438a622bae7d8652848a1fc2
ypham5 <ypham5@nait.ca>
2020-02-12 18:48:46 -0700

    head or tail done

---
+++ Practising-exercises/head-tail-solution/head-tail-loop/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace head_tail_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputValue;
+            int loopCounter = 1, head = 0, tail = 0;
+            while (loopCounter <= 7)
+            {
+                Console.Write("Enter the H or T: ");
+                inputValue = Console.ReadLine();
+                switch (inputValue.ToUpper())
+                {
+                    case "H":
+                        {
+                            head++;
+                            break;
+                        }
+                    case "T":
+                        {
+                            tail++;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Entered invalid value");
+                            break;
+                        }
+                }
+                loopCounter++;
+
+            }//eol
+
+            Console.WriteLine($"\nThe even number count is {head}");
+            Console.WriteLine($"The odd number count is {tail}\n\n");
+        }
+    }
+}
+
