4541bcae36bdf6c49399966956846985cf25ba55
ypham5 <59590820+ypham5@users.noreply.github.com>
2020-03-11 18:41:05 -0600

    demo for random and question 4 done

---
+++ ClassDemos/Array/Question4/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Question4
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            int[] driverArray = new int[25];
+            int logicalsize = ReadandLoadArray(driverArray, 25);
+            int sum = 0;
+            for (int index = 0; index < logicalsize; index++)
+            {
+                sum += driverArray[index];
+            }
+            if (logicalsize > 0)
+            {
+                string name = "bob";
+                Console.WriteLine($"Quiz Total: {logicalsize}");
+                Console.WriteLine($"Student      Mark");
+                Console.WriteLine($"_______      ____");
+                int index = 0;
+                while (index < logicalsize)
+                {
+                    Console.Write($"{name,-7}");
+                    Console.WriteLine($"{driverArray[index]}".PadLeft(9));
+                    index++;
+                }
+                FindHighandLow(driverArray, logicalsize);
+                Console.WriteLine($"The average of the {logicalsize} numbers is {(double)sum/ (double)logicalsize}");
+            }
+            else
+            {
+                Console.WriteLine("You have no numbers to calculate an average");
+            }
+            Console.ReadKey();
+        }
+        static int ReadandLoadArray(int[] myArray, int physicalsize)
+        {
+
+            int logicalsize = 0;
+            string Full_Path_File_Name = "";
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            Full_Path_File_Name = fd.FileName;
+            string readValue = "";
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader(Full_Path_File_Name);
+                readValue = reader.ReadLine();
+                while (readValue != null && logicalsize < physicalsize)
+                {
+                    myArray[logicalsize] = int.Parse(readValue);
+                    logicalsize++;
+
+                    //get the next line
+                    readValue = reader.ReadLine();
+                }
+
+            } //eof try
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+            return logicalsize;
+        }
+
+        static void FindHighandLow(int[] myArray, int logicalsize)
+        {
+            int highest = 0;
+            int lowest = 100;
+
+            int loopCounter = 0;
+            while (loopCounter < logicalsize)
+            {
+                if (highest < myArray[loopCounter])
+                {
+                    highest = myArray[loopCounter];
+                }
+                if (lowest > myArray[loopCounter])
+                {
+                    lowest = myArray[loopCounter];
+                }
+                loopCounter++;
+            }
+            Console.WriteLine($"highest mark is {highest}");
+            Console.WriteLine($"lowest mark is {lowest}");
+        }
+    }
+}
---
+++ ClassDemos/Array/RandomGen/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomGen
+{
+    class Program
+    {
+        //if declared outside of a method add the keyword static in front
+        // of your declaration
+        //if declared outside of a method BUT within the class The the 
+        //variable is known to ALL methods WITHOUT having to pass
+        //the value as a parameter
+        //static Random rnd = new Random(); if you wanna random outside must go with static
+        static void Main(string[] args)
+        {
+            //Random is a datatype of class (oject)
+            //Random is just the definition of the object (decribes the ojbect)
+            //Random is NOT a physical instance of the object
+            //To get a physical instance of the object use the keyboard new
+            //new is used by any object to create an instance of that object 
+            //To identify the object instance datatype, add the class name after the keyword new
+            //Random is datatype 
+            //rnd is the variable name of the object instance
+            //new is the command keyword to create an object instance 
+            //Random() is the type of object instance new to create
+            Random rnd = new Random();
+            int[] myArray = new int[10];
+            for (int i=0; i<10; i++)
+            {
+                myArray[i] = rnd.Next(1, 100);
+                Console.WriteLine($"index {i} has a value of {myArray[i]}");
+            }
+
+            //what is the highish random number generated and the lowest random number generated 
+            int highest = 0;
+            int lowest = 100;
+
+            int loopCounter = 0;
+            while(loopCounter<10)
+            {
+                if (highest < myArray[loopCounter])
+                {
+                    highest = myArray[loopCounter];
+                }
+                if (lowest > myArray[loopCounter])
+                {
+                    lowest = myArray[loopCounter];
+                }
+                loopCounter++;
+            }
+            Console.WriteLine($"highest value is {highest}");
+            Console.WriteLine($"lowest value is {lowest}");
+        }
+        //static void SomeMethod (Random paramRnd) datatype + variable and random is datatype 
+    }
+}
